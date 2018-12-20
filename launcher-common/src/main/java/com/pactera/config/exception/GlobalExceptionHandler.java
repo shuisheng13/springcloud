0d91f688ed973532138808ec8cdfbaaaa0fa0deb
xukj <xukj@autoai.com>
2018-12-20 09:36:14 +0800

    增加对于nullpointer处理

--- launcher-common/src/main/java/com/pactera/config/exception/GlobalExceptionHandler.java
+++ launcher-common/src/main/java/com/pactera/config/exception/GlobalExceptionHandler.java
@@ -1,18 +1,26 @@
 package com.pactera.config.exception;
 
-import javax.servlet.http.HttpServletResponse;
-
+import com.pactera.config.exception.status.ErrorStatus;
+import com.pactera.result.ResultData;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.pactera.result.ResultData;
+import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
+
+	@ExceptionHandler(value = NullPointerException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<ResultData> NullPointerHandler(Exception exception)  {
+        exception.printStackTrace();
+	    return new ResponseEntity<>(new ResultData(exception.getMessage(), ErrorStatus.PARAMETER_ERROR),HttpStatus.BAD_REQUEST);
+	}
+
 	@ExceptionHandler(value = Exception.class)
 	public ResponseEntity<ResultData> defaultErrorHandler(HttpServletRequest request,HttpServletResponse response,Exception exception)  {
 		exception.printStackTrace();
