d9d2c3f713531a14ea43cbfa4da76d7b452800c4
xukj <xukj@autoai.com>
2018-12-28 15:24:52 +0800

    主题上传zip

---
+++ launcher-domain/src/main/java/com/pactera/vo/LaunThemeUploadFileVo.java
@@ -0,0 +1,34 @@
+package com.pactera.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @ClassName LaunThemeUploadFileVo
+ * @Description 上传文件后的返回数据格式
+ * @Author xukj
+ * @Date 2018/12/26 15:37
+ * @Version
+ */
+@Data
+public class LaunThemeUploadFileVo {
+
+    /**
+     *  主题预览图片（最大4张）
+     */
+    private List<LaunThemeFileVo> themeImgsList;
+    /**
+     *  主题分辨率宽
+     */
+    private String wideResolution;
+    /**
+     *  主题分辨率长
+     */
+    private String longResolution;
+
+    /**
+     * 压缩包路径
+     */
+    private String zipUrl;
+}
