01388f8323b26ac2848c8a0d415a160ca8332976
xukj <xukj@autoai.com>
2018-12-28 15:24:14 +0800

    增加io异常处理

---
+++ launcher-common/src/main/java/com/pactera/config/exception/IORuntimeException.java
@@ -0,0 +1,14 @@
+package com.pactera.config.exception;
+
+/**
+ * @ClassName IORuntimeException
+ * @Description
+ * @Author xukj
+ * @Date 2018/12/28 10:19
+ * @Version
+ */
+public class IORuntimeException extends RuntimeException{
+    public IORuntimeException(Throwable e) {
+        super(e);
+    }
+}
