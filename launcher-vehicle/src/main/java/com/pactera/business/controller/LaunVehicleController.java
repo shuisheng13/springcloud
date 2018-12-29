7f1627755ec749ff43a6b1a8610fb2e75877d054
zhaodong <zhaodong@autoai.com>
2018-12-29 11:15:17 +0800

    添加

---
+++ launcher-vehicle/src/main/java/com/pactera/business/controller/LaunVehicleController.java
@@ -0,0 +1,15 @@
+package com.pactera.business.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author zhaodong
+ * @Description
+ * @Date 2018/12/29 10:55
+ */
+@RestController
+public class LaunVehicleController {
+
+
+}
