02242db1c19bdaabce05e0e1ad85fa9811322861
xukj <xukj@autoai.com>
2019-01-02 16:43:13 +0800

    注释权限

--- launcher-system/src/main/java/com/pactera/config/interceptor/InterceptorConfig.java
+++ launcher-system/src/main/java/com/pactera/config/interceptor/InterceptorConfig.java
@@ -1,19 +1,19 @@
-package com.pactera.config.interceptor;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import com.pactera.config.interceptor.LogsInterceptor;
-
-
-@Configuration
-public class InterceptorConfig extends WebMvcConfigurerAdapter{
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry){
-		//日志的拦截器
-		registry.addInterceptor(new LogsInterceptor()).addPathPatterns("/**");
-		super.addInterceptors(registry);
-	}
-	
-}
+//package com.pactera.config.interceptor;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import com.pactera.config.interceptor.LogsInterceptor;
+//
+//
+//@Configuration
+//public class InterceptorConfig extends WebMvcConfigurerAdapter{
+//
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry){
+//		//日志的拦截器
+//		registry.addInterceptor(new LogsInterceptor()).addPathPatterns("/**");
+//		super.addInterceptors(registry);
+//	}
+//
+//}
