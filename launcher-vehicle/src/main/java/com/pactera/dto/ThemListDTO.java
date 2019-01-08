bef1fc79b6636704b19442193a46fc52c4b250bf
zhaodong <zhaodong@autoai.com>
2019-01-08 14:48:54 +0800

    添加实体

---
+++ launcher-vehicle/src/main/java/com/pactera/dto/ThemListDTO.java
@@ -0,0 +1,21 @@
+package com.pactera.dto;
+
+import lombok.Data;
+
+/**
+ * @Author zhaodong
+ * @Date 10:21 2019/1/7
+ **/
+@Data
+public class ThemListDTO {
+
+    private String previewPath;
+
+    private String title;
+
+    private int downCount;
+
+    private String id;
+
+    private String price;
+}
