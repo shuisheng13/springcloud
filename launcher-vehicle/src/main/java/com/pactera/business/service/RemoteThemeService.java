ae1e26b66787ec553e005ed03930938b34238dd6
xukj <xukj@autoai.com>
2019-01-17 13:47:51 +0800

    服务器配置

--- launcher-vehicle/src/main/java/com/pactera/business/service/RemoteThemeService.java
+++ launcher-vehicle/src/main/java/com/pactera/business/service/RemoteThemeService.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @Version
  */
 
-@FeignClient(url="http://127.0.0.1:9091/", name="system")
+@FeignClient("launcher-service")
 public interface RemoteThemeService {
 
     @RequestMapping(method = RequestMethod.GET, value = "/theme/detail/{id}")
