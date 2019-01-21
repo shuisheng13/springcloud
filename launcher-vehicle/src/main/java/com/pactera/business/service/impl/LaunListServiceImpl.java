d22a0be06ee5fd857e76b4c13f85b3d1d1892f27
zhaodong <zhaodong@autoai.com>
2019-01-21 17:07:03 +0800

    添加

--- launcher-vehicle/src/main/java/com/pactera/business/service/impl/LaunListServiceImpl.java
+++ launcher-vehicle/src/main/java/com/pactera/business/service/impl/LaunListServiceImpl.java
@@ -4,7 +4,6 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.navinfo.wecloud.common.rest.response.CommonResult;
 import com.navinfo.wecloud.saas.api.facade.ApiKeyFacade;
-import com.navinfo.wecloud.saas.api.facade.TenantFacade;
 import com.navinfo.wecloud.saas.api.response.TenantInfo;
 import com.pactera.business.dao.LaunVehicleListMapper;
 import com.pactera.business.service.LaunListService;
@@ -14,10 +13,8 @@ import com.pactera.dto.ThemClassDTO;
 import com.pactera.dto.ThemListDTO;
 import com.pactera.result.ResultData;
 import com.pactera.vo.LaunPage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.Resource;
 import java.util.List;
 
@@ -74,7 +71,7 @@ public class LaunListServiceImpl implements LaunListService {
         int tenanId = data.getId();
         PageHelper.startPage(pageNum, pageSize);
         LaunThemeAdministration LaunThemeVo = new LaunThemeAdministration();
-        LaunThemeVo.setTenantId(tenanId+"");
+        LaunThemeVo.setTenantId(tenanId);
         if (status==1) LaunThemeVo.setSort(-1);// 全部标记
         else if (status==2) LaunThemeVo.setDownloadCount(-1);// 排行标记
         else if (status==3) LaunThemeVo.setRecommendSort(-1);// 推荐标记
@@ -83,7 +80,6 @@ public class LaunListServiceImpl implements LaunListService {
         PageInfo<ThemListDTO> PageInfo = new PageInfo<>(themListDTOS);
         JSONObject json = new JSONObject();
         json.put("list",new LaunPage(PageInfo, themListDTOS));
-
         ResultData resultData = new ResultData(json);
         return ResponseEntity.ok().body(resultData);
     }
@@ -105,7 +101,7 @@ public class LaunListServiceImpl implements LaunListService {
         }
         int tenanId = data.getId();
         LaunThemeAdministration LaunThemeVo = new LaunThemeAdministration();
-        LaunThemeVo.setTenantId(tenanId+"");
+        LaunThemeVo.setTenantId(tenanId);
         LaunThemeVo.setTypeId(id); // id
         LaunThemeVo.setDownloadCount(-1);//分类的主题列表按照下载量排序
         List<ThemListDTO> themListDTOS = launVehicleListMapper.themTopAndByClassId(LaunThemeVo);
