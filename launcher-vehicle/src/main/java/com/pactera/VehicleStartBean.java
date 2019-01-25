0250be0551188178757734dc1477422baa5bdf89
zhaodong <zhaodong@autoai.com>
2019-01-25 11:38:23 +0800

    重构

--- launcher-vehicle/src/main/java/com/pactera/VehicleStartBean.java
+++ launcher-vehicle/src/main/java/com/pactera/VehicleStartBean.java
@@ -7,12 +7,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
+
 @SpringBootApplication
 @MapperScan(basePackages = "com.pactera.business.dao")
 @Configuration
 @EnableDiscoveryClient
 @EnableFeignClients({"com.navinfo.wecloud.saas.api.facade","com.pactera.business.service"})
-
 public class VehicleStartBean {
 
     public static void main(String[] args) {
