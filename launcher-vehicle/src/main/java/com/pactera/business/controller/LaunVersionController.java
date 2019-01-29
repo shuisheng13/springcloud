f6fefd09bc974a753089b7c9e3661bf8c82ce9d9
xukj <xukj@autoai.com>
2019-01-29 18:08:05 +0800

    增加校验

--- launcher-vehicle/src/main/java/com/pactera/business/controller/LaunVersionController.java
+++ launcher-vehicle/src/main/java/com/pactera/business/controller/LaunVersionController.java
@@ -4,6 +4,7 @@ import com.pactera.business.service.LaunVersionService;
 import com.pactera.result.ResultData;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2019/1/7 14:18
  * @Version
  */
+@Validated
 @RestController
 @RequestMapping("/version")
 public class LaunVersionController {
