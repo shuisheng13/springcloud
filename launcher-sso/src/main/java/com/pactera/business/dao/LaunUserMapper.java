f99aa733f5febae195460c1d28e8f2b768329231
xukj <xukj@autoai.com>
2019-01-29 15:47:54 +0800

    配置文件修改

--- launcher-sso/src/main/java/com/pactera/business/dao/LaunUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.pactera.business.dao;
-
-import com.pactera.config.mapper.BaseMapper;
-import com.pactera.domain.LaunUser;
-
-public interface LaunUserMapper extends BaseMapper<LaunUser> {
-
-}
