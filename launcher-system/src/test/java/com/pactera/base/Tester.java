3be951b24e1cc374e4a8cf92213c8910e1a39f98
xukuijian <lovejoefirst@sina.cn>
2019-03-13 13:46:20 +0800

    单元测试 范例

--- launcher-system/src/test/java/com/pactera/base/Tester.java
+++ launcher-system/src/test/java/com/pactera/base/Tester.java
@@ -1,13 +1,10 @@
 package com.pactera.base;
 
 
-
-
+import com.pactera.SystemStartBean;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.pactera.SystemStartBean;
 
 /**
  * 单元测试继承该类即可
