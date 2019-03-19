177fd8a35dec56264c48508e5617bc7e71d61b6f
zhaodong <zhaodong@autoai.com>
2019-03-19 15:32:35 +0800

    bug

--- launcher-vehicle/src/main/java/com/pactera/business/service/impl/LaunListServiceImpl.java
+++ launcher-vehicle/src/main/java/com/pactera/business/service/impl/LaunListServiceImpl.java
@@ -13,6 +13,7 @@ import com.pactera.dto.ThemClassDTO;
 import com.pactera.dto.ThemListDTO;
 import com.pactera.result.ResultData;
 import com.pactera.vo.LaunPage;
+import com.pactera.vo.ThemeParaVO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -46,13 +47,6 @@ public class LaunListServiceImpl implements LaunListService {
      **/
     @Override
     public ResponseEntity<ResultData> themeclasslist2(String apiKey, String layoutName){
-       /* CommonResult<TenantInfo> tenantInfoCommonResult = apiKeyFacade.queryTenantByApiKey(apiKey);
-        TenantInfo data = tenantInfoCommonResult.getData();
-        if (data==null){
-            return ResponseEntity.ok().body(new ResultData(ErrorStatus.NOT_APIKEY.status(),ErrorStatus.NOT_APIKEY.message()));
-        }
-        int tenanId = data.getId();*/
-        //int tenanId = 55;
         List<ThemClassDTO> themListDTOS = launVehicleListMapper.themeclasslist2(layoutName);
         themListDTOS.stream().filter(t->!t.getCoverImage().contains(fastUrl)).forEach(t->t.setCoverImage(fastUrl+t.getCoverImage()));
         JSONObject json = new JSONObject();
@@ -70,22 +64,13 @@ public class LaunListServiceImpl implements LaunListService {
      **/
     @Override
     public ResponseEntity<ResultData> themTopAndAll(String apiKey,int status, int pageNum, int pageSize,double version,String layoutName){
-       /* CommonResult<TenantInfo> tenantInfoCommonResult = apiKeyFacade.queryTenantByApiKey(apiKey);
-        TenantInfo data = tenantInfoCommonResult.getData();
-        if (data==null){
-            return ResponseEntity.ok().body(new ResultData(ErrorStatus.NOT_APIKEY.status(),ErrorStatus.NOT_APIKEY.message()));
-        }
-        int tenanId = data.getId();*/
-        //int tenanId = 55;
         PageHelper.startPage(pageNum, pageSize);
-        Map<String,Object> map =new HashMap();
-       // LaunThemeAdministration LaunThemeVo = new LaunThemeAdministration();
-        map.put("version",version);
-        if (status==1) map.put("sort",-1);// 全部标记
-        else if (status==2) map.put("downloadCount",-1);// 排行标记
-        else if (status==3) map.put("recommendSort",-1);// 推荐标记
+        ThemeParaVO para = new ThemeParaVO().setVersion(version).setLayoutName(layoutName);
+        if (status==1) para.setSort(-1);// 全部标记
+        else if (status==2) para.setDownloadCount(-1);// 排行标记
+        else if (status==3) para.setRecommendSort(-1);// 推荐标记
         else return ResponseEntity.ok().body(new ResultData(ErrorStatus.SYS_ERROR.status(),ErrorStatus.SYS_ERROR.message()));// 暂时提供系统错误
-        List<ThemListDTO> themListDTOS = launVehicleListMapper.themTopAndByClassId(map);
+        List<ThemListDTO> themListDTOS = launVehicleListMapper.themTopAndByClassId(para);
         themListDTOS.stream().filter(t->!t.getPreviewPath().contains(fastUrl)).forEach(t->t.setPreviewPath(fastUrl+t.getPreviewPath()));
         PageInfo<ThemListDTO> PageInfo = new PageInfo<>(themListDTOS);
         JSONObject json = new JSONObject();
@@ -104,21 +89,8 @@ public class LaunListServiceImpl implements LaunListService {
     @Override
     public ResponseEntity<ResultData> themTopAndByClassId(String apiKey, String id, int pageNum, int pageSize,double version){
         PageHelper.startPage(pageNum, pageSize);
-        /*CommonResult<TenantInfo> tenantInfoCommonResult = apiKeyFacade.queryTenantByApiKey(apiKey);
-        TenantInfo data = tenantInfoCommonResult.getData();
-        if (data==null){
-            return ResponseEntity.ok().body(new ResultData(ErrorStatus.NOT_APIKEY.status(),ErrorStatus.NOT_APIKEY.message()));
-        }
-        if (id==null){
-            return ResponseEntity.ok().body(new ResultData("id不为空"));
-        }
-        int tenanId = data.getId();*/
-        //int tenanId = 55;
-        Map<String,Object> map =new HashMap();
-        map.put("typeId",id);// id
-        map.put("downloadCount",-1);//分类的主题列表按照下载量排序
-        map.put("version",version);
-        List<ThemListDTO> themListDTOS = launVehicleListMapper.themTopAndByClassId(map);
+        ThemeParaVO para = new ThemeParaVO().setTypeId(id).setDownloadCount(-1).setVersion(version);
+        List<ThemListDTO> themListDTOS = launVehicleListMapper.themTopAndByClassId(para);
         themListDTOS.stream().filter(t->!t.getPreviewPath().contains(fastUrl)).forEach(t->t.setPreviewPath(fastUrl+t.getPreviewPath()));
         PageInfo<ThemListDTO> PageInfo = new PageInfo<>(themListDTOS);
         JSONObject json = new JSONObject();
